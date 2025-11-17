131ba4a417634d2499010210c86fcfeb270acb7b
jlbuild <juliabuildbot@gmail.com>
2025-11-17 18:57:17 +0000

    qiskit_ibm_runtime_jll build 0.38.0+0

---
+++ src/qiskit_ibm_runtime_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule qiskit_ibm_runtime_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("qiskit_ibm_runtime")
+JLLWrappers.@generate_main_file("qiskit_ibm_runtime", UUID("dfd00f80-54b8-5b12-a44d-54bafd549557"))
+end  # module qiskit_ibm_runtime_jll
